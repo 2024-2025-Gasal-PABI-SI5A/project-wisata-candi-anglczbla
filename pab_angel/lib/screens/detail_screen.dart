c9abf4266daed4fb4b5c50136d069780d6407ead
angel <angelicaizabella90@gmail.com>
2024-11-09 11:57:23 +0700

    push

---
+++ pab_angel/lib/screens/detail_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:pab_angel/models/candi.dart';
+
+class DetailScreen extends StatelessWidget{
+  final Candi candi;
+  
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Scaffold(
+    body: Column(
+      children: [],
+    ),
+   );
+  }
+
+}
